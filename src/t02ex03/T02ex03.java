75388c3598877a77b410301fc8086e9e5ba8be0d
Rodolfo Lazo <rodolfo.lazo@outlook.com>
2021-01-26 10:37:13 +0000

    Commit inicial

---
+++ src/t02ex03/T02ex03.java
@@ -0,0 +1,23 @@
+/*
+ * Floating A
+ */
+package t02ex03;
+
+/**
+ * @author rodo
+ */
+public class T02ex03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int iNumber = 5;
+        float pi = 3.1415f;
+        
+        System.out.println(pi/iNumber);
+        
+    }
+    
+}
